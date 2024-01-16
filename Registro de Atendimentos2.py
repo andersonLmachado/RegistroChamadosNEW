e92fcc41cc158c3f413af4f258abd72506c1eb6d
Anderson Machado <88006912+andersonLmachado@users.noreply.github.com>
2024-01-16 10:41:41 -0300

    Add files via upload

--- Registro de Atendimentos2.py
+++ Registro de Atendimentos2.py
@@ -10,7 +10,9 @@ class RegistroAtendimentos(Ctk.CTk):
         
         set_default_color_theme('green')
         
-        self.label_titulo = Ctk.CTkLabel(self, text="Registro de Atendimentos", font=("Helvetica", 16, "bold"))
+        self.frame_titulo = Ctk.CTkFrame(self)
+        
+        self.label_titulo = Ctk.CTkLabel(master=self.frame_titulo, text="Registro de Atendimentos", font=("Helvetica", 16, "bold"))
         
         # Contadores 
         self.contador_suzano1 = 0
@@ -23,36 +25,66 @@ class RegistroAtendimentos(Ctk.CTk):
         self.contador_suzano8 = 0
         
         ## Declarar empresa ##
-        self.label_suzano = Ctk.CTkLabel(self, text=f"Suzano", font=("Helvetica", 16, "bold"))
+
+        self.frame_suzano = Ctk.CTkFrame(self)
+        
+        self.label_suzano = Ctk.CTkLabel(master=self.frame_suzano, text=f"Suzano", font=("Helvetica", 16, "bold"))
 
         ## Declarar botões ##
-        self.btn_suzano1 = Ctk.CTkButton(master=self, text="Cadastro Omnilink", command=self.SuzanoContador1)
-        self.label_suzano1 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano2 = Ctk.CTkButton(master=self, text="Verificar Espelhamento", command=self.SuzanoContador2)
-        self.label_suzano2 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano3 = Ctk.CTkButton(master=self, text="Trocar Rastreador", command=self.SuzanoContador3)
-        self.label_suzano3 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano4 = Ctk.CTkButton(master=self, text="Voltar para PNA", command=self.SuzanoContador4)
-        self.label_suzano4 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano5 = Ctk.CTkButton(master=self, text="Treinamento", command=self.SuzanoContador5)
-        self.label_suzano5 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano6 = Ctk.CTkButton(master=self, text="Cadastro", command=self.SuzanoContador6)
-        self.label_suzano6 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano7 = Ctk.CTkButton(master=self, text="Dúvidas", command=self.SuzanoContador7)
-        self.label_suzano7 = Ctk.CTkLabel(master=self, text="0")
-        
-        self.btn_suzano8 = Ctk.CTkButton(master=self, text="Outros", command=self.SuzanoContador8)
-        self.label_suzano8 = Ctk.CTkLabel(master=self, text="0")
+        self.btn_suzano1 = Ctk.CTkButton(master=self.frame_suzano, text="Cadastro Omnilink", command=self.SuzanoContador1)
+        self.label_suzano1 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
 
+        self.btn_suzano2 = Ctk.CTkButton(master=self.frame_suzano, text="Verificar Espelhamento", command=self.SuzanoContador2)
+        self.label_suzano2 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano3 = Ctk.CTkButton(master=self.frame_suzano, text="Trocar Rastreador", command=self.SuzanoContador3)
+        self.label_suzano3 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano4 = Ctk.CTkButton(master=self.frame_suzano, text="Voltar para PNA", command=self.SuzanoContador4)
+        self.label_suzano4 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano5 = Ctk.CTkButton(master=self.frame_suzano, text="Treinamento", command=self.SuzanoContador5)
+        self.label_suzano5 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano6 = Ctk.CTkButton(master=self.frame_suzano, text="Cadastro", command=self.SuzanoContador6)
+        self.label_suzano6 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano7 = Ctk.CTkButton(master=self.frame_suzano, text="Dúvidas", command=self.SuzanoContador7)
+        self.label_suzano7 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.btn_suzano8 = Ctk.CTkButton(master=self.frame_suzano, text="Outros", command=self.SuzanoContador8)
+        self.label_suzano8 = Ctk.CTkLabel(master=self.frame_suzano, text="0")
+
+        self.frame_suzano.grid(row=4, column=0, pady=10, padx=10)
+        
+        self.btn_suzano1.grid(row=0, column=0, pady=10, padx=10)
+        self.label_suzano1.grid(row=0, column=1, pady=10, padx=10)
+        
+        self.btn_suzano2.grid(row=1, column=0, pady=10, padx=10)
+        self.label_suzano2.grid(row=1, column=1, pady=10, padx=10)
+        
+        self.btn_suzano3.grid(row=2, column=0, pady=10, padx=10)
+        self.label_suzano3.grid(row=2, column=1, pady=10, padx=10)
+        
+        self.btn_suzano4.grid(row=3, column=0, pady=10, padx=10)
+        self.label_suzano4.grid(row=3, column=1, pady=10, padx=10)
+        
+        self.btn_suzano5.grid(row=4, column=0, pady=10, padx=10)
+        self.label_suzano5.grid(row=4, column=1, pady=10, padx=10)
+        
+        self.btn_suzano6.grid(row=5, column=0, pady=10, padx=10)
+        self.label_suzano6.grid(row=5, column=1, pady=10, padx=10)
+        
+        self.btn_suzano7.grid(row=6, column=0, pady=10, padx=10)
+        self.label_suzano7.grid(row=6, column=1, pady=10, padx=10)
+        
+        self.btn_suzano8.grid(row=7, column=0, pady=10, padx=10)
+        self.label_suzano8.grid(row=7, column=1, pady=10, padx=10)
+        
         ## Posicionamento dos botões ##
         
         #Título
+        self.frame_titulo.grid(row=0, column=6, pady=10, padx=10)
         self.label_titulo.grid(row=0, column=6, pady=10, padx=10)
         
         #Suzano
@@ -114,6 +146,7 @@ class RegistroAtendimentos(Ctk.CTk):
     def SuzanoContador8(self):
         self.contador_suzano8 += 1
         self.label_suzano8.configure(text=f"{self.contador_suzano8}")
+
                   
 if __name__ == '__main__':
     app = RegistroAtendimentos()
